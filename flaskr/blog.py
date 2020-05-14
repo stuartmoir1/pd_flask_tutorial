e7b1258935feaab3ce723293e593365882cfecc0
Stuart Moir <stuart.a.moir@live.com>
2020-05-14 18:49:24 +0100

    Adds support for update view

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -49,4 +49,43 @@ def create():
          return redirect(url_for('blog.index'))
 
    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+   post = get_db().execute(
+   ).fetchone()
+
+   if post is None:
+      # 404 not found
+      abort(404, 'Post id {0} doesn\'t exist'.format(id))
+   if check_author and post['author_id'] != g.user['id']:
+      # 403 forbidden
+      abort(403)
+
+   return post
+
+@bp.route('<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+   post = get_post(id)
    
+   if request.method == 'POST':
+      title = request.form['title']
+      body = request.form['body']
+      error = None
+
+      if not title:
+         error = 'Title is required'
+
+      if error is not None:
+         flash(error)
+      else:
+         db = get_db()
+         db.execute(
+            'UPDATE post SET title = ?, body = ?'
+            ' WHERE id = ?',
+            (title, body, id)
+         )
+         db.commit()
+         return redirect(url_for('blog.index'))
+
+   return render_template('blog/update.html', post=post)

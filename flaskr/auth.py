f55eb67038047c67b3873fa15ec066879c3afa0c
Stuart Moir <stuart.a.moir@live.com>
2020-05-14 16:45:58 +0100

    Adds formatting

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -7,12 +7,12 @@ from flask import (
    redirect,
    render_template,
    request,
-   session
+   session,
    url_for
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
-from flasker.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -29,7 +29,7 @@ def register():
       elif not password:
          error = 'Password is required'
       elif db.execute(
-         'SELECT id FROM user WHERE username = ?', (username)
+         'SELECT id FROM user WHERE username = ?', (username,)
       ).fetchone() is not None:
          error = 'User {} us already registered.'.format(username)
 
@@ -54,7 +54,7 @@ def login():
       error = None
 
       user = db.execute(
-         'SELECT * FROM user WHERE username = ?', (username)
+         'SELECT * FROM user WHERE username = ?', (username,)
       ).fetchone()
 
       if user is None:
@@ -78,7 +78,7 @@ def load_logged_in_user():
       g.user = None
    else:
       g.user = get_db().execute(
-         'SELECT * FROM user WHERE id = ?', (user_id)
+         'SELECT * FROM user WHERE id = ?', (user_id,)
       ).fetchone()
 
 @bp.route('/logout')

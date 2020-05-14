98aa71a9b2bf88aad9515e02ef33db9d597da2ef
Stuart Moir <stuart.a.moir@live.com>
2020-05-14 13:00:59 +0100

    Adds file __init__.py

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+# application factory function
+def create_app(test_config=None):
+   # create and configure app
+   app = Flask(__name__, instance_relative_config=True)
+   app.config.from_mapping(
+      SECRET_KEY = 'dev',
+      DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+   )
+
+   if test_config is None:
+      # load instance config, it it exists, when not testing
+      app.config.from_pyfile('config.py', silent=True)
+   else:
+      # load test config if passed in
+      app.config.from_mapping(test_config)
+
+   # ensure instance folder exists
+   try:
+      os.makedirs(app.instance_path)
+   except OSError:
+      pass
+
+   # simple page that says hello
+   @app.route('/hello')
+   def hello():
+      return 'Hello, World!'
+
+   return app

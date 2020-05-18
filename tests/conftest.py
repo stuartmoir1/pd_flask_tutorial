1ffefb383e7adfa678af64a321393ce36c01a924
Stuart Moir <stuart.a.moir@live.com>
2020-05-18 13:20:47 +0100

    Resolves error in conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from flaskr import create_app
 from flaskr.db import get_db, init_db
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql', 'rb')) as f:
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
    _data_sql = f.read().decocde('utf8')
 
 @pytest.fixture

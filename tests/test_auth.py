d9d3c5cff58db9b56d48b83b55a22ccd50a376b4
Stuart Moir <stuart.a.moir@live.com>
2020-05-18 09:02:25 +0100

    Adds login authorisation tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -28,3 +28,23 @@ def test_register_validate_input(client, username, password, message):
       data={ 'username': username, 'password':, password }
    )
    assert message in response.data
+
+def test_login(client, auth):
+   assert client.get('/auth/login').status_code == 200
+   response = auth.login()
+   assert response.headers['Location'] == 'http://localhost/'
+
+   with client:
+      client.get('/')
+      assert session['user_id'] == 1
+      assert g.user['username'] == 'test'
+
+@pytest.mark.parameterize(
+   ('username', 'password', 'message'),
+   (
+      ('a', 'test', b'Incorrect username.'),
+      ('test', 'a', b'Incorrect password')
+   ))
+def test_login_validate_input(auth, username, password, message):
+   response = auth.login(username, password)
+   assert message in response.data
